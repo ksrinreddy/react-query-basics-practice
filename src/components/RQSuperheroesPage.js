0b41028274e8fc05d7d48a03b85150f2680969f0
ksrinreddy <ksrinreddy@gmail.com>
2023-05-28 19:01:39 +0530

    fetch RQSuperheroes data using basic react-query

--- src/components/RQSuperheroesPage.js
+++ src/components/RQSuperheroesPage.js
@@ -1,12 +1,29 @@
+import axios from "axios";
 import React from "react";
+import { useQuery } from "react-query";
+import Loading from "./Loading";
 import { Container } from "./styles/Container.styled";
 import { RQSuperHeroes } from "./styles/RQSuperHeroes.styled";
 
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
 const RQSuperHeroesPage = () => {
+  const { isLoading, data } = useQuery("super-heroes", fetchSuperHeroes);
+  // console.log(data);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <RQSuperHeroes>
       <Container>
         <h3>RQ Super Heroes Page</h3>
+        {data?.data.map((hero) => (
+          <div key={hero.id}>{hero.name}</div>
+        ))}
       </Container>
     </RQSuperHeroes>
   );
