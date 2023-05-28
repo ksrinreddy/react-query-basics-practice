20d323bea0b5adf73a7b266f6874ab1b50496616
ksrinreddy <ksrinreddy@gmail.com>
2023-05-28 18:57:23 +0530

    create Loading.js

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledLoading } from "./styles/Loading.styled";
+
+const Loading = () => {
+  return (
+    <StyledLoading>
+      <h2>Loading...</h2>
+    </StyledLoading>
+  );
+};
+
+export default Loading;
