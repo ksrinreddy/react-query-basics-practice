7d5a2b902759d805c5b5461736a5ba55d7bcc956
ksrinreddy <ksrinreddy@gmail.com>
2023-05-28 13:16:08 +0530

    structure navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Container } from "./styles/Container.styled";
+import { StyledNavbar } from "./styles/Navbar.styled";
+
+const Navbar = () => {
+  return (
+    <StyledNavbar>
+      <Container>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/super-heroes">Tradition Super Heroes</Link>
+          </li>
+          <li>
+            <Link to="/rq-super-heroes">RQ Super Heores</Link>
+          </li>
+        </ul>
+      </Container>
+    </StyledNavbar>
+  );
+};
+
+export default Navbar;
