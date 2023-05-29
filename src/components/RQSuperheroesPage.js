520149b5d3227076fc2fab4facf6785b5bf1d2b5
ksrinreddy <ksrinreddy@gmail.com>
2023-05-29 08:24:57 +0530

    update fetch function name

--- src/components/RQSuperheroesPage.js
+++ src/components/RQSuperheroesPage.js
@@ -5,12 +5,12 @@ import Loading from "./Loading";
 import { Container } from "./styles/Container.styled";
 import { RQSuperHeroes } from "./styles/RQSuperHeroes.styled";
 
-const fetchSuperHeroes = () => {
+const fetchRQSuperHeroes = () => {
   return axios.get("http://localhost:4000/superheroes");
 };
 
 const RQSuperHeroesPage = () => {
-  const { isLoading, data } = useQuery("super-heroes", fetchSuperHeroes);
+  const { isLoading, data } = useQuery("super-heroes", fetchRQSuperHeroes);
   // console.log(data);
 
   if (isLoading) {
@@ -21,9 +21,9 @@ const RQSuperHeroesPage = () => {
     <RQSuperHeroes>
       <Container>
         <h3>RQ Super Heroes Page</h3>
-        {data?.data.map((hero) => (
-          <div key={hero.id}>{hero.name}</div>
-        ))}
+        {data?.data.map((hero) => {
+          return <div key={hero.id}>{hero.name}</div>;
+        })}
       </Container>
     </RQSuperHeroes>
   );
