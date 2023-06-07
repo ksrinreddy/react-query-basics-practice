8c6ac862d2bd1c74fba10a2c3b3d0831cf10318c
ksrinreddy <ksrinreddy@gmail.com>
2023-06-07 19:35:18 +0530

    get familiar with handling mutation response

--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -24,8 +24,12 @@ export const useSuperHeroesData = (onSuccess, onError) => {
 export const useAddSuperHeroData = () => {
   const queryClient = useQueryClient();
   return useMutation(addSuperHero, {
-    onSuccess: () => {
-      queryClient.invalidateQueries("super-heroes");
+    onSuccess: (data) => {
+      // queryClient.invalidateQueries("super-heroes");
+      // console.log(data);
+      queryClient.setQueryData("super-heroes", (oldQueryData) => {
+        return { ...oldQueryData, data: [...oldQueryData.data, data.data] };
+      });
     },
   });
 };
